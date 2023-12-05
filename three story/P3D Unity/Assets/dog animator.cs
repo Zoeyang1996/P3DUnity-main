45769915e8067805c23c783824a41d74c162bfbe
YYZZGg <yZ1059509907@gamil.com>
2023-12-05 13:37:02 +0000

    dog movement 12 05

--- three story/P3D Unity/Assets/dog animator.cs
+++ three story/P3D Unity/Assets/dog animator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class dogmove : MonoBehaviour
 {
     Animator animator;
-    public float speed = 2.0f;
+    [SerializeField] private float speed = 2.0f;
+    [SerializeField] private float turnSpeed = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class dogmove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // move forward 
         if (Input.GetKey("w"))
         {
         animator.SetBool("isWalking", true);
@@ -26,6 +27,32 @@ public class dogmove : MonoBehaviour
         if (!Input.GetKey("w"))
         {
             animator.SetBool("isWalking", false);
+
+        }
+        // turn left
+        if (Input. GetKey("a"))
+        {
+            animator.SetBool("isLeft", true);
+            // float rotateValue = Vector3.down * turnSpeed * Time.deltaTime;
+            // Debug.Log("rotate value " + rotateValue);
+            // transform.Rotate(rotateValue);
+            transform.Rotate(Vector3.down * turnSpeed * Time.deltaTime);
+        }
+        if (!Input.GetKey("a"))
+        {
+            animator.SetBool("isLeft", false);
+
+        }
+        
+        // turn right
+        if (Input.GetKey("d"))
+        {
+            animator.SetBool("isRight", true);
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+        }
+        if (!Input.GetKey("d"))
+        {
+            animator.SetBool("isRight", false);
 
         }
 
